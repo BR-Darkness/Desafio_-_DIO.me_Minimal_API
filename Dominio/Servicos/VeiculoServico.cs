7a1fa14aac07543b363f13b06d5a44d9a430a924
BR-Darkness <BRvitorgalindo@gmail.com>
2025-08-10 07:13:42 -0300

    feat: finalizada aula - Configurando modelo de veículos

---
+++ Dominio/Servicos/VeiculoServico.cs
@@ -0,0 +1,64 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalAPI.Dominio.Entidades;
+using MinimalAPI.Dominio.Interfaces;
+using MinimalAPI.DTOs;
+using MinimalAPI.Infraestrutura.Db;
+
+namespace MinimalAPI.Dominio.Servicos
+{
+    public class VeiculoServico : IVeiculoServico
+    {
+        private readonly DbContexto _contexto;
+
+        public VeiculoServico(DbContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Apagar(Veiculo veiculo)
+        {
+            _contexto.Veiculos.Remove(veiculo);
+            _contexto.SaveChanges();
+        }
+
+        public void Atualizar(Veiculo veiculo)
+        {
+            _contexto.Veiculos.Update(veiculo);
+            _contexto.SaveChanges();
+        }
+
+        public Veiculo? BuscaPorId(int id)
+        {
+            return _contexto.Veiculos.Where(
+                veiculo => veiculo.Id == id
+            ).FirstOrDefault();
+        }
+
+        public void Incluir(Veiculo veiculo)
+        {
+            _contexto.Veiculos.Add(veiculo);
+            _contexto.SaveChanges();
+        }
+
+        public List<Veiculo> Todos(int pagina = 1, string? nome = null, string? marca = null)
+        {
+            IQueryable<Veiculo> query = _contexto.Veiculos.AsQueryable();
+            
+            if(!string.IsNullOrEmpty(nome))
+            {
+                query = query.Where(
+                    veiculo => EF.Functions.Like(
+                        veiculo.Nome.ToLower(),
+                        $"%{nome}%"
+                    )
+                );
+            }
+
+            int itensPorPagina = 10;
+
+            query = query.Skip((pagina - 1) * itensPorPagina).Take(itensPorPagina);
+
+            return query.ToList();
+        }
+    }
+}
